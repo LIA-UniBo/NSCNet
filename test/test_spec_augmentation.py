7d40354398644b0ebe711d0e20307af63c450697
Daniele Sirocchi <daniele.sirocchi@xonne.it>
2022-04-08 09:28:00 +0200

    Started to write a possible test for the SpecAugmentation class (work in progress).

---
+++ test/test_spec_augmentation.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+import numpy as np
+
+from architectures.spec_augmentation import time_warp
+
+
+class TestSpecAugmentation(TestCase):
+    def test_time_warp(self):
+        size = 10
+
+        x = np.zeros((10, 10), dtype=np.float32)
+
+        for i in range(0, size):
+            elem = np.arange(i*size+1, i*size+1+size, dtype=np.float32)
+            x[i, :] = elem
+
+        x = np.expand_dims(x, axis=-1)
+
+        time_warp(x, 4)
+
+        print()
